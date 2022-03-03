5c84a6d8f267cc73cbf186c613fa303caca2505e
CharanGhanta <89552449+CharanGhanta@users.noreply.github.com>
2022-03-03 17:53:05 -0600

    Add files via upload

---
+++ Ghanta_SearchApp/Ghanta_SearchApp/Ghanta_SearchApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ghanta_SearchApp
+//
+//  Created by Ghanta,Sai Charan on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var searchButtonAction: UIButton!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var showNextImagesBtn: UIButton!
+    
+    @IBOutlet weak var ShowPrevImagesBtn: UIButton!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
